edead8bdca92f4010ed7e2921042d98a3fa2bce0
NoahSchmidt2003 <67223583+NoahSchmidt2003@users.noreply.github.com>
2020-08-31 20:35:09 +0200

    Update TwitchBot.py

--- TwitchBot.py
+++ TwitchBot.py
@@ -1,67 +1,69 @@
 import asyncio
+import json
 import sys
 import requests
 import discord
-from discord import member
-
-try:
-    reload(sys)
-    sys.setdefaultencoding('utf8')
-except Exception:
-    pass
+from discord import *
 
 
-class MyClient(discord.Client):
+def Twitch():
+    global live, viewercount, username, game_id, title, startedat, thumbnail, livebool
+
+    apitoken = "https://id.twitch.tv/oauth2/token?client_id=Your ID&client_secret" \
+               "=Your clientsecret&grant_type=client_credentials&scope="
+    API = 'https://api.twitch.tv/helix/streams?user_login=your streamer of choice'
+    Client_ID = "Your Client ID"
+    rq_token = requests.post(url=apitoken)
+    tokendic = json.loads(rq_token.text)
+    token = tokendic['access_token']
+    head = {
+        'client-id': Client_ID,
+        "Authorization": "Bearer " + token
+    }
+    rq = requests.get(url=API, headers=head)
+    rqdic = json.loads(rq.text)
+    print(rqdic)
+    try:
+        live = rqdic['data'][0]['type']
+    except:
+        livebool = False
+        return livebool
+    viewercount = str(rqdic['data'][0]['viewer_count'])
+    username = str(rqdic['data'][0]['user_name'])
+    game_id = str(rqdic['data'][0]['game_id'])
+
+    title = str(rqdic['data'][0]['title'])
+    startedat = str(rqdic['data'][0]['started_at'])
+    thumbnail = str(rqdic['data'][0]['thumbnail_url'])
+    thumbnail = thumbnail[:thumbnail.find('{')]
+    thumbnail = str(thumbnail + "1920x1080.jpg")
+    livebool = True
+
+    return live, viewercount, username, game_id, title, startedat, thumbnail
+
+
+class BotClient(discord.Client):
 
     async def on_ready(self):
-        print("Ich habe mich eingeloggt")
-
-    async def on_member_join(self):
-        await member.create_dm()
-        await member.dm_channel.send(
-            "Herzlich Wikommen " + str(member.name) + " auf den Community Discord Server von gammagames")
+        print("Bot is ready")
 
     async def on_message(self, message):
+        firstmsg = "@everyone "
         if message.content.startswith("start bot"):
-            try:
-                with open('oauth.txt', 'r') as f:
-                    oauth = f.read()
-            # Store OAuth in oauth.txt
-            except FileNotFoundError:
-                print("Paste your OAuth (like 'yaxb50....')")
-                with open('oauth.txt', 'w') as f:
-                    f.write(input())
-                with open('oauth.txt', 'r') as f:
-                    oauth = f.read()
-
-            headers = {
-                'Accept': 'application/vnd.twitchtv.v5+json',
-                'Client-ID': '',
-                'Authorization': 'OAuth ' + oauth,
-            }
             while True:
-                try:
-                    response = requests.get('https://api.twitch.tv/kraken/streams/followed', headers=headers)
-                    data = response.json()
-                except (KeyError, ValueError):
-                    print("Error - make sure your OAuth is formatted correctly in oauth.txt")
-                    sys.exit(1)
-                print(data)
-                numStreams = len(data['streams'])
-                for i in range(0, numStreams):
-                    name = data["streams"][i]["channel"]["name"]
-                    game = data["streams"][i]["channel"]["game"]
-                    viewers = str(data["streams"][i]["viewers"])
-                    stream = data["streams"][i]["stream_type"]
-
-                if stream == "live":
+                Twitch()
+                if livebool:
                     messages = (
-                            name + " ist nun live und spielt " + game + " aktuell hat er " + viewers + " Zuschauer" + "\nhttps://www.twitch.tv/daannyy")
-                    message = await message.channel.send(messages)
-                    await asyncio.sleep(30)
-                    await message.delete()
+                            "" + firstmsg + "Streamer :red_circle: is live :red_circle: " + title + "with "
+                                                                                                              ":ok_hand: " + viewercount + " vieweres :ok_hand: \nSometext \nLink from the streamer ")
+                    firstmsg = ""
+                else:
+                    messages = "streamer is acutally  :black_circle:  offline :black_circle: !"
+                message = await message.channel.send(messages)
+                await asyncio.sleep(300)
+                await message.delete()
 
 
 if __name__ == '__main__':
-    bot = MyClient()
-    bot.run("Your bot token")
+    bot = BotClient()
+    bot.run("Your discord bot token")
